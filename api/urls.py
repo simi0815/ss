639fcfde4c7ad2870b0a1ddfd0e4cfb01dd43457
mac <simi@qq.com>
2021-04-11 20:31:11 +0800

    清空venv

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from .views import InfoList, TOXML, SearchByFile, SearchPost, CheckFile,FileStatus
+from api.views import InfoList, TOXML, SearchByFile, SearchPost, CheckFile,FileStatus
 
 urlpatterns = [
     re_path(r'^(?P<version>[v1|v2]+)/infolist$', InfoList.as_view()),
